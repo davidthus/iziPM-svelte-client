0da5e0fca972b188ffbeed983393d1765f6f9d7a
davidthus <dvdnicorici05@gmail.com>
2023-06-04 14:45:49 +0100

    add: query client to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <script>
+	import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query';
 	// Your selected Skeleton theme:
 	import '@skeletonlabs/skeleton/themes/theme-vintage.css';
-
 	// This contains the bulk of Skeletons required styles:
 	// NOTE: this will be renamed skeleton.css in the v2.x release.
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
-
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.postcss';
+
+	const queryClient = new QueryClient();
 </script>
 
-<slot />
+<QueryClientProvider client={queryClient}>
+	<slot />
+</QueryClientProvider>
