15d8e3b212d3bc466c6aaa8b2f08d3ea2f81cf38
davidthus <dvdnicorici05@gmail.com>
2023-06-07 21:17:17 +0100

    add: import auth store into fetchFunctions

--- src/features/users/fetchFunctions.ts
+++ src/features/users/fetchFunctions.ts
@@ -1,5 +1,16 @@
 import axiosBase from '../../app/api';
+import { auth } from '../auth/auth';
+
+let authObj: { token: string; userId: string };
+
+auth.subscribe((value) => {
+	authObj = value;
+});
 
 export function getUser() {
-	axiosBase.get('/users', { headers: {} });
+	axiosBase.get('/users', {
+		headers: {
+			authorization: `Bearer ${authObj.token}`
+		}
+	});
 }
