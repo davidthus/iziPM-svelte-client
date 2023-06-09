c91e2d48724efaa43dedcbca46d2e59b8e400092
davidthus <dvdnicorici05@gmail.com>
2023-06-09 23:14:27 +0100

    add: updateUser to user FetchFunctions

--- src/features/users/fetchFunctions.ts
+++ src/features/users/fetchFunctions.ts
@@ -1,6 +1,17 @@
 import axiosBase from '../../app/api';
 import { auth } from '../auth/auth';
 
+interface InewUserInfo {
+	userId: string;
+	avatar?: {
+		data: Buffer;
+		contentType: string;
+	};
+	password?: string;
+	notes: string;
+	username: string;
+}
+
 let authObj: { accessToken: string; userId: string };
 
 auth.subscribe((value) => {
@@ -13,4 +24,8 @@ export function getUser() {
 			authorization: `Bearer ${authObj.accessToken}`
 		}
 	});
+}
+
+export function updateUser(newUserInfo: InewUserInfo) {
+	axiosBase.patch('/users', { data: { ...newUserInfo } });
 }
