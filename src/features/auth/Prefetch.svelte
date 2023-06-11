61801d05811d2bf0415f8cbd6ae396ce9f756410
davidthus <dvdnicorici05@gmail.com>
2023-06-11 12:36:53 +0100

    add: prefetch component

--- src/features/auth/Prefetch.svelte
+++ src/features/auth/Prefetch.svelte
@@ -1,2 +1,13 @@
 <script lang="ts">
+	import { useQuery } from '@sveltestack/svelte-query';
+	import { onMount } from 'svelte';
+	import { getUserProjects } from '../projects/queryFunctions';
+	import { getUser } from '../users/queryFunctions';
+
+	onMount(() => {
+		useQuery('user', getUser);
+		useQuery('userProjects', getUserProjects);
+	});
 </script>
+
+<slot />
