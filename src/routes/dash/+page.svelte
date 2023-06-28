18441849336fcd46c59ef54385d464a7f2fcb112
davidthus <dvdnicorici05@gmail.com>
2023-06-28 22:12:59 +0100

    add: query in dash home page

---
+++ src/routes/dash/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { useQuery } from '@sveltestack/svelte-query';
+	import { getUserProjects } from '../../features/projects/queryFunctions';
+
+	const projectsQuery = useQuery('userProjects', getUserProjects);
+	const projectsList = projectsQuery.data.projects;
+</script>
+
+<main>
+	<ul />
+</main>
