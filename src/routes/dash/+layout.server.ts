6605549b17b87c655e27cff84ee1c433e98aae5c
davidthus <dvdnicorici05@gmail.com>
2023-07-04 16:23:52 +0100

    add: profile picture section

--- src/routes/dash/+layout.server.ts
+++ src/routes/dash/+layout.server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { queryClient } from '$lib/query';
 import type { RequestEvent } from '@sveltejs/kit';
 import { fail } from '@sveltejs/kit';
--- src/routes/dash/+page.svelte
+++ src/routes/dash/+page.svelte
@@ -11,31 +11,53 @@
 	};
 
 	const projectsQuery = useQuery<ProjectsQueryResult>('userProjects', getUserProjects);
-	const projectsList = projectsQuery.data?.projects;
+	const projectsList = $projectsQuery.data?.projects;
 	const user = useQuery('user', getUser);
 
 	const tasksToDoWithinAWeek = projectsList
-		.map((project: IProject) => {
-			return project.tasks.filter((task) => Number(task.dueDate) < Date.now() + weekInMilliseconds);
-		})
-		.flat()
-		.filter((task: ITask) =>
-			task.assignedTo.some((member: IUser) => member._id === user.data?.user._id)
-		);
+		? projectsList
+				.map((project: IProject) => {
+					return project.tasks.filter(
+						(task) => Number(task.dueDate) < Date.now() + weekInMilliseconds
+					);
+				})
+				.flat()
+				.filter((task: ITask) =>
+					task.assignedTo.some((member: IUser) => member._id === $user.data?.user._id)
+				)
+		: null;
 
-	const mostUrgentTasks = tasksToDoWithinAWeek.sort(
-		(a: any, b: any) => Number(a.dueDate) - Number(b.dueDate)
-	);
+	const mostUrgentTasks = tasksToDoWithinAWeek
+		? tasksToDoWithinAWeek.sort((a: any, b: any) => Number(a.dueDate) - Number(b.dueDate))
+		: null;
+
+	const { avatar } = $user.data;
+	// Convert the binary data to a base64-encoded data URI
+	const dataURI =
+		'data:' +
+		avatar.contentType +
+		';base64,' +
+		btoa(String.fromCharCode(...new Uint8Array(avatar.data)));
 </script>
 
 <main>
 	<ul>
-		{#each mostUrgentTasks as task}
-			<div>
-				<a href={`/dash/projects/${task.projectId}/task/${task._id}`}>
-					<p>{task.name}</p>
-				</a>
-			</div>
-		{/each}
+		{#if mostUrgentTasks}
+			{#each mostUrgentTasks as task}
+				<div>
+					<a href={`/dash/projects/${task.projectId}/task/${task._id}`}>
+						<p>{task.name}</p>
+					</a>
+				</div>
+			{/each}
+		{/if}
 	</ul>
+	<section>
+		<img src={dataURI} alt="User profile picture" />
+		<form action="">
+			<p>{$user?.data.username}</p>
+			<p>{$user?.data.email}</p>
+			<input type="password" bind:value={$user.data.password} />
+		</form>
+	</section>
 </main>
