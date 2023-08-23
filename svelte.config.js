e98fa30a1e3cdc72300acdad7fc0f5378ae698f4
davidthus <dvdnicorici05@gmail.com>
2023-08-23 21:29:23 +0100

    create: path aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [
 		vitePreprocess(),
 		preprocess({
@@ -14,10 +12,18 @@ const config = {
 	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			'@/*': './src/*',
+			components: './src/components',
+			api: './src/app/api.ts',
+			features: './src/features',
+			constants: './src/constants/index.ts',
+			hooks: './src/hooks',
+			lib: './src/lib',
+			types: './src/types',
+			util: './src/util'
+		}
 	}
 };
 
