4bdc04d701d7a5006bb60e66b570fb2ed1ff1672
davidthus <dvdnicorici05@gmail.com>
2023-09-06 21:29:15 +0100

    move: sidebar markup directly into layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,9 @@
 
 	import '@skeletonlabs/skeleton/themes/theme-vintage.css';
 	// NOTE: this will be renamed skeleton.css in the v2.x release.
+	import { sendLogout } from '@/features/auth/queryFunctions';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	import Sidebar from 'components/Sidebar.svelte';
+	import { superForm } from 'sveltekit-superforms/client';
 	import '../app.postcss';
 
 	const isSidebarOpen = writable(true);
@@ -16,9 +17,31 @@
 
 	$: ({ data } = $page);
 	console.log($page);
+	// Client API:
+	$: ({ form, errors } = superForm(data.newProjectForm));
 </script>
 
 {#if $isSidebarOpen}
-	<Sidebar {toggleSidebar} {data} />
+	{#if data.user}
+		<header>
+			<div>
+				<form action="/actions?/createProject" method="POST">
+					<input type="text" name="projectName" bind:value={$form.projectName} />
+					{#if $errors.projectName}
+						<p>{$errors.projectName}</p>
+					{/if}
+					<button type="submit">Create project</button>
+				</form>
+			</div>
+			<div>
+				<form action="/actions?/logout" method="post">
+					<button title="Logout" type="submit" on:click={sendLogout}>
+						<div class="logout icon" />
+					</button>
+				</form>
+				<button title="Close Sidebar" on:click={toggleSidebar}>Close sidebar</button>
+			</div>
+		</header>
+	{/if}
 {/if}
 <slot />
