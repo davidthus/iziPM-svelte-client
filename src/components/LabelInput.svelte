a162c13d07f53a619578901efb0fa45d2f771b43
davidthus <dvdnicorici05@gmail.com>
2023-09-16 22:07:07 +0100

    change labelinput props

--- src/components/LabelInput.svelte
+++ src/components/LabelInput.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	export let value: string;
+	export let form: any;
 	export let name: string;
 </script>
 
 <label for={name} class="flex flex-col">
 	<slot />
-	<input class="text-primary-900 bg-surface-800" type="text" {name} bind:value />
+	<input class="text-primary-200 bg-surface-800" type="text" {name} bind:value={$form[name]} />
 </label>
--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -15,9 +15,9 @@
 	</header>
 	<main>
 		<form class="form" method="post">
-			<LabelInput value={$form.username} name="username">Username</LabelInput>
-			<LabelInput value={$form.email} name="email">Email</LabelInput>
-			<LabelInput value={$form.password} name="password">Password</LabelInput>
+			<LabelInput {form} name="username">Username</LabelInput>
+			<LabelInput {form} name="email">Email</LabelInput>
+			<LabelInput {form} name="password">Password</LabelInput>
 
 			<button type="submit">Sign up</button>
 		</form>
